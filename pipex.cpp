230b4c49d6b5f1828c032840ce8c28085f03196a
Mark Williamsen <39509081+springleik@users.noreply.github.com>
2023-07-27 10:52:45 +0200

    Update pipex.cpp
    Added white space for readability.

--- pipex.cpp
+++ pipex.cpp
@@ -1,5 +1,5 @@
 // pipex.cpp sends structs with flexible array members through a pipe
-// M. Williamsen, Quantum Design, 7 November 2022
+// M. Williamsen, 7 November 2022
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -7,29 +7,36 @@
 #include <iostream>
 #include <cstring>
 #include <iomanip>
+
 using namespace std;
+
 // global variables, duplicated in the child process
 FILE *rFile, *wFile;
 char buff[256] = {0};
 pid_t pid;
 int firstPipe[2], secondPipe[2];
+
 // WARNING These classes end with a flexible array member,
 // so they must never be instantiated!
 // The intended use is to point to a region of memory which
 // contains data laid out according to the member list.
 // NOTE There are no virtual methods in the base class.
 // suppress padding in subclasses
+
 #pragma pack(2)
+
 // base class for all packet types
 struct whatBase
 {
     // instance method
     void showHex(ostream &os);
+
     // member list for memory layout (8 bytes total)
     int length;         // 4 bytes, size including subclass
     enum typeEnum{typeA = 10,
         typeB} type;    // 4 bytes, type of subclass
 };
+
 void whatBase::showHex(ostream &os)
 {
     unsigned char *p = reinterpret_cast<unsigned char *>(this);
@@ -41,28 +48,33 @@ void whatBase::showHex(ostream &os)
     }
     os << "\n]" << dec << setfill(' ');
 }
+
 // subclass with real-value members (20 bytes plus string)
 struct whatA: public whatBase
 {
     // instance methods
     void populate(float a, double b, const string &c);
     void serialize(ostream &os);
+
     // member list for memory layout, without trailing null
     float theFlt;   // 4 bytes
     double theDbl;  // 8 bytes
     char theStr[];  // flexible array member (must be last)
 };
+
 // subclass with integer-value members (14 bytes plus string)
 struct whatB: public whatBase
 {
     // instance methods
     void populate(short a, int b, const string &c);
     void serialize(ostream &os);
+
     // member list for memory layout, without trailing null
     short theShort; // 2 bytes
     int theInt;     // 4 bytes
     char theStr[];  // flexible array member (must be last)
 };
+
 // initialization method for type A
 void whatA::populate(float a, double b, const string &c)
 {
@@ -73,6 +85,7 @@ void whatA::populate(float a, double b, const string &c)
         cerr << "Bad string size: " << cSize << endl;
         return;
     }
+    
     // copy data members into memory, without trailing null
     length = sizeof(whatA) + cSize;
     type = typeA;
@@ -80,6 +93,7 @@ void whatA::populate(float a, double b, const string &c)
     theDbl = b;
     c.copy(theStr, cSize);
 }
+
 // initialization method for type B
 void whatB::populate(short a, int b, const string &c)
 {
@@ -90,6 +104,7 @@ void whatB::populate(short a, int b, const string &c)
         cerr << "Bad string size: " << cSize << endl;
         return;
     }
+    
     // copy data members into memory, without trailing null
     length = sizeof(whatB) + cSize;
     type = typeB;
@@ -97,6 +112,7 @@ void whatB::populate(short a, int b, const string &c)
     theInt = b;
     c.copy(theStr, cSize);
 }
+
 // report method for type A
 void whatA::serialize(ostream &os)
 {
@@ -106,6 +122,7 @@ void whatA::serialize(ostream &os)
         os << "Bad struct size: " << length << ", pid: " << pid << endl;
         return;
     }
+    
     // show struct data members first
     int cSize = length - sizeof(whatA);
     string cStr(theStr, cSize);
@@ -115,10 +132,12 @@ void whatA::serialize(ostream &os)
         << ",\"theFlt\":" << theFlt
         << ",\"theDbl\":" << theDbl
         << ",\"theStr\":\"" << cStr << "\"";
+    
     // then show buffer contents as hex bytes
     showHex(os);
     os << '}' << endl;
 }
+
 // report method for type B
 void whatB::serialize(ostream &os)
 {
@@ -128,6 +147,7 @@ void whatB::serialize(ostream &os)
         os << "Bad struct size: " << length << ", pid: " << pid << endl;
         return;
     }
+    
     // show struct data members first
     int cSize = length - sizeof(whatB);
     string cStr(theStr, cSize);
@@ -137,10 +157,12 @@ void whatB::serialize(ostream &os)
         << ",\"theShort\":" << theShort
         << ",\"theInt\":" << theInt
         << ",\"theStr\":\"" << cStr << "\"";
+    
     // then show buffer contents as hex bytes
     showHex(os);
     os << '}' << endl;
 }
+
 void doParentStuff()
 {
     // parent process
@@ -148,6 +170,7 @@ void doParentStuff()
     close(secondPipe[1]);
     wFile = fdopen(firstPipe[1], "w");
     rFile = fdopen(secondPipe[0], "r");
+    
     // iterate over lines of user input
     string theString;
     do  {
@@ -156,22 +179,27 @@ void doParentStuff()
         cout << "\nType a text string: ";
         getline(cin, theString);
         if (!theString.size()) {break;}
+        
         // allocate and populate a type A instance
         whatA *myWhatA = reinterpret_cast<whatA *>(buff);
         myWhatA->populate(1.234e5, 2.345e67, theString);
+        
         // write out to child
         fwrite(buff, 1, myWhatA->length, wFile);
         fflush(wFile);
         myWhatA->serialize(cout);
         memset(buff, 0, sizeof(buff));
+        
         // allocate and populate a type B instance
         whatB *myWhatB = reinterpret_cast<whatB *>(buff);
         myWhatB->populate(0x1234, 0x123456, theString);
+        
         // write out to child
         fwrite(buff, 1, myWhatB->length, wFile);
         fflush(wFile);
         myWhatB->serialize(cout);
         memset(buff, 0, sizeof(buff));
+        
         // read back from child, reuse same buffer
         fread(buff, 1, 4, rFile);
         fread(buff + 4, 1, myWhatA->length - 4, rFile);
@@ -185,6 +213,7 @@ void doParentStuff()
     fclose(wFile);
     fclose(rFile);
 }
+
 void doChildStuff()
 {
     // child process
@@ -192,6 +221,7 @@ void doChildStuff()
     close(secondPipe[0]);
     rFile = fdopen(firstPipe[0], "r");
     wFile = fdopen(secondPipe[1], "w");
+    
     // iterate over packets sent from parent
     // fread() blocks until parent closes the pipe
     do  {
@@ -199,6 +229,7 @@ void doChildStuff()
         whatBase *myWhat = reinterpret_cast<whatBase *>(buff);
         if (!fread(buff, 1, 4, rFile)) {break;}
         if (!fread(buff + 4, 1, myWhat->length - 4, rFile)) {break;}
+        
         // check packet type, modify values accordingly
         switch (myWhat->type)
         {
@@ -223,20 +254,24 @@ void doChildStuff()
             default:
                 cout << "Unknown type: " << myWhat->type << endl;
         }
+        
         // write the instance back out, common to all packet types
         fwrite(buff, 1, myWhat->length, wFile);
         fflush(wFile);
         memset(buff, 0, sizeof(buff));
     }   while (true);
+    
     // clean up and exit
     fclose(rFile);
     fclose(wFile);
 }
+
 int main()
 {
     // check struct packing
     cout << "Type A is 20 bytes without string: " << sizeof(whatA)
         << "\nType B is 14 bytes without string: " << sizeof(whatB) << endl;
+    
     // open two anonymous pipes
     if (pipe(firstPipe))
     {
@@ -248,6 +283,7 @@ int main()
         cout << "Failed to open second (inbound) pipe." << endl;
         return -1;
     }
+    
     // fork into two processes
     pid = fork();
     if (!pid) {doChildStuff();}
