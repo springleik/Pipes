05a73fcbfecb9b8d70da2f6356ac250dfc1cf3c1
Mark Williamsen <39509081+springleik@users.noreply.github.com>
2023-07-27 10:55:33 +0200

    Update pipes.cpp
    Added white space for readability.

--- pipes.cpp
+++ pipes.cpp
@@ -1,5 +1,5 @@
 // pipes.cpp sends structs with flexible array members through a pipe
-// M. Williamsen, Quantum Design, 4 November 2022
+// M. Williamsen, 4 November 2022
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -7,12 +7,15 @@
 #include <iostream>
 #include <cstring>
 #include <iomanip>
+
 using namespace std;
+
 // global variables, duplicated in the child process
 FILE *rFile, *wFile;
 char buff[256] = {0};
 pid_t pid;
 int firstPipe[2], secondPipe[2];
+
 // WARNING This struct ends with a flexible array member,
 // so it must never be instantiated!  The intended use
 // is to point to a region of memory which contains
@@ -22,12 +25,14 @@ struct what
     // instance methods
     void populate(float a, double b, const string &c);
     void show(ostream &os);
+
     // member list for memory layout, without trailing null
     int len;        // 4 bytes, must contain actual size
     float one;      // 4 bytes
     double two;     // 8 bytes
     char three[];   // flexible array member (must be last)
 };
+
 // implementation of initialization method
 void what::populate(float a, double b, const string &c)
 {
@@ -38,12 +43,14 @@ void what::populate(float a, double b, const string &c)
         cerr << "Bad string size: " << cSize << endl;
         return;
     }
+    
     // copy data members into memory, without trailing null
     len = sizeof(what) + cSize;
     one = a;
     two = b;
     strncpy(three, c.c_str(), cSize);
 }
+
 // implementation of report method
 void what::show(ostream &os)
 {
@@ -53,6 +60,7 @@ void what::show(ostream &os)
         os << "Bad struct size: " << len << ", pid: " << pid << endl;
         return;
     }
+    
     // show struct data members first
     int cSize = len - sizeof(what);
     string cStr(three, cSize);
@@ -60,6 +68,7 @@ void what::show(ostream &os)
         << ", one: " << one
         << ", two: " << two
         << ", three: '" << cStr << "'";
+    
     // then show buffer contents as hex bytes
     unsigned char *p = reinterpret_cast<unsigned char *>(this);
     os << hex << setfill('0');
@@ -70,6 +79,7 @@ void what::show(ostream &os)
     }
     os << dec << setfill(' ') << endl;
 }
+
 void doParentStuff()
 {
     // parent process
@@ -77,6 +87,7 @@ void doParentStuff()
     close(secondPipe[1]);
     wFile = fdopen(firstPipe[1], "w");
     rFile = fdopen(secondPipe[0], "r");
+    
     // iterate over lines of user input
     string theString;
     do  {
@@ -85,23 +96,28 @@ void doParentStuff()
         cout << "\nType a text string: ";
         getline(cin, theString);
         if (!theString.size()) {break;}
+        
         // allocate and populate an instance
         what *myWhat = reinterpret_cast<what *>(buff);
         myWhat->populate(1.234e5, 2.345e67, theString);
+        
         // write out to child
         fwrite(buff, 1, myWhat->len, wFile);
         fflush(wFile);
         myWhat->show(cout);
         memset(buff, 0, sizeof(buff));
+        
         // read back from child
         fread(buff, 1, 4, rFile);
         fread(buff + 4, 1, myWhat->len - 4, rFile);
         myWhat->show(cout);
     }   while (true);
+    
     // clean up and exit
     fclose(wFile);
     fclose(rFile);
 }
+
 void doChildStuff()
 {
     // child process
@@ -109,6 +125,7 @@ void doChildStuff()
     close(secondPipe[0]);
     rFile = fdopen(firstPipe[0], "r");
     wFile = fdopen(secondPipe[1], "w");
+    
     // iterate over structs sent from parent
     do  {
         what *myWhat = reinterpret_cast<what *>(buff);
@@ -124,10 +141,12 @@ void doChildStuff()
         fflush(wFile);
         memset(buff, 0, sizeof(buff));
     }   while (true);
+    
     // clean up and exit
     fclose(rFile);
     fclose(wFile);
 }
+
 int main()
 {
     // open two anonymous pipes
@@ -141,6 +160,7 @@ int main()
         cout << "Failed to open second (inbound) pipe." << endl;
         return -1;
     }
+    
     // fork into two processes
     pid = fork();
     if (!pid) {doChildStuff();}
